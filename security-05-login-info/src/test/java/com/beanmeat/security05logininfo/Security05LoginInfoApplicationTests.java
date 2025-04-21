095bde9edad794b5a3bbd18bbaf4b45be98a927c
3090018282@qq.com <3090018282@qq.com>
2025-04-21 18:50:36 +0800

    security login-info

---
+++ security-05-login-info/src/test/java/com/beanmeat/security05logininfo/Security05LoginInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beanmeat.security05logininfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Security05LoginInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
