be748be915d9a7b6fca825b2b927a7aaf9ddb5eb
3090018282@qq.com <3090018282@qq.com>
2025-04-21 14:37:18 +0800

    security captcha-login init

---
+++ security-04-captcha-login/src/main/java/com/beanmeat/security/SecurityCaptchaApplication.java
@@ -0,0 +1,13 @@
+package com.beanmeat.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityCaptchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityCaptchaApplication.class, args);
+	}
+
+}
